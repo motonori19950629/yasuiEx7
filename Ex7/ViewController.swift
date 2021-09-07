612a7fe05f33ff8c6e01b852340b391049daf9b7
motonori19950629 <s2.k5.i2.a2.a@ezweb.ne.jp>
2021-09-07 18:29:19 +0900

    Initial Commit

---
+++ Ex7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ex7
+//
+//  Created by 安井基哲 on 2021/09/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
